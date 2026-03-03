01c90f68e392913667cad7e4cf4c99c4ac4a1041
Mayur Pande <mayurgpande@gmail.com>
2026-03-03 12:25:01 +0530

    implamented media query

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
@@ -8,24 +7,34 @@ export const Hero = () => {
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Mayur</h1>
+
         <p className={styles.description}>
-          Entry level Software Developer with hands-on experience in React, JavaScript, SQL, and Core Java.
-          Understanding of Data Structures, OOP and DBMS with practical exposure to building front-end projects and writing optimized SQL queries.
+          Entry level Software Developer with hands-on experience in React,
+          JavaScript, SQL, and Core Java. Understanding of Data Structures,
+          OOP and DBMS with practical exposure to building front-end projects
+          and writing optimized SQL queries.
         </p>
-        <a
-  href="https://mail.google.com/mail/?view=cm&fs=1&to=mayurgpande@gmail.com"
-  target="_blank"
-  rel="noopener noreferrer"
-  className={styles.contactBtn}
->
-  Contact
-</a>
+
+        <div className={styles.btnContainer}>
+          <a href="https://mail.google.com/mail/?view=cm&fs=1&to=mayurgpande@gmail.com" 
+          target="_blank" rel="noopener noreferrer" className={styles.contactBtn}>Hire Me</a>
+          <a
+            href="/resume.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.contactBtn}
+          >
+            Resume
+          </a>
+        </div>
       </div>
+
       <img
         src={getImageUrl("hero/img.png")}
         alt="Hero image of me"
         className={styles.heroImg}
       />
+
       <div className={styles.topBlur} />
       <div className={styles.bottomBlur} />
     </section>
