07c301de950bc59d8dd2ca65c1f78f2a4a77b400
Mayur Pande <mayurgpande@gmail.com>
2026-03-12 10:57:18 +0530

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import styles from "./App.module.css";
+import {Navbar} from "./components/Navbar/Navbar";
 import {Hero} from "./components/Hero/Hero"
 import {About} from "./components/About/About";
-import { Contact } from "./components/Contact/Contact";
-import {Navbar} from "./components/Navbar/Navbar";
 import {Projects} from "./components/Projects/Projects"
 import { Skills } from "./components/Skills/Skills";
 import { Certificates } from "./components/Certificate/Certificates";
-
-
+import { Contact } from "./components/Contact/Contact";
 
 function App() {
   return (
@@ -22,5 +20,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
